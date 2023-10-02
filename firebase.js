bce2811c27feec8cc8f5a7de1c5a36ea44d378e3
Mohammad Al-amin <129145554+mohammadalamin2k23@users.noreply.github.com>
2023-10-02 21:53:54 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+var app_firebase = {};
+(function () { 
+    var firebaseConfig = {
+        apiKey: "AIzaSyBvjkoV_7wLjjAazYFPmMBngZSmk2GGMWM",
+    authDomain: "p13-35715.firebaseapp.com",
+    projectId: "p13-35715",
+    storageBucket: "p13-35715.appspot.com",
+    messagingSenderId: "236496439114",
+    appId: "1:236496439114:web:ec804622245e48c97723b3",
+    measurementId: "G-SVDWFKY65M"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  app_firebase = firebase;
+  firebase.analytics();
+})()
